b0b5b90e1bad2410fea936a389b08e1c8985126d
kqcoxn <mjy03730@163.com>
2025-06-20 23:26:51 +0800

    feat: 二级导航

--- docs/.vitepress/navs.ts
+++ docs/.vitepress/navs.ts
@@ -1,13 +1,24 @@
 export const zhNav = [
   { text: "主页", link: "/" },
   { text: "应用开发", link: "/application/1.1-QuickStarted" },
-  { text: "Binding接口", link: "/binding/NodeJS/J1.1-快速开始" },
+  {
+    text: "Binding接口",
+    items: [
+      {
+        text: "NodeJS",
+        link: "/binding/NodeJS/J1.1-快速开始",
+      },
+    ],
+  },
   { text: "框架开发", link: "/framework/4.1-BuildGuide" },
 ];
 
 export const enNav = [
   { text: "Home", link: "/en/" },
   { text: "Application Dev", link: "/en/application/1.1-QuickStarted" },
-  { text: "Binding Interface", link: "/en/binding/NodeJS/J1.1-快速开始" },
+  {
+    text: "Binding Interface",
+    items: [{ text: "NodeJS", link: "/en/binding/NodeJS/J1.1-快速开始" }],
+  },
   { text: "Framework Dev", link: "/en/framework/4.1-BuildGuide" },
 ];
